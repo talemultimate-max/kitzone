cf84bce3b2cd28a3aa3cd2ecf2e7e4786efe3405
Cristiano <cristiano@email.com>
2026-08-02 02:37:53 +0200

    Aggiunto images al tipo Kit

--- src/types/kit.ts
+++ src/types/kit.ts
@@ -3,6 +3,7 @@ export interface Kit {
   name: string;
   price: number;
   image?: string;
+  images?: string[]; // Aggiunto per gestire più immagini
   description?: string;
   category?: string;
   team?: string;
